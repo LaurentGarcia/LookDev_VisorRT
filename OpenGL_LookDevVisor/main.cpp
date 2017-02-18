baa7bff61314a5c00bcfe739f08e06e90ab73e9a
Lauren Garcia <laurent.garcia.carro@gmail.com>
2017-02-18 00:26:56 -0600

    updating and testing environment

--- OpenGL_LookDevVisor/main.cpp
+++ OpenGL_LookDevVisor/main.cpp
@@ -5,6 +5,7 @@
 #include <glfw3.h>
 
 
+
 int main()
 {
 	glfwInit();
@@ -14,12 +15,29 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LookDev Visor", nullptr, nullptr);
-	if (window = nullptr){
+	if (window == nullptr){
 		std::runtime_error("Imposible to create the window");
 		glfwTerminate();
+		return -1;
 	};
 	glfwMakeContextCurrent(window);
 	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK){
+		std::runtime_error("Can't not possible init GLEW");
+		glfwTerminate;
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	while (!glfwWindowShouldClose(window)){
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	};
+
+	glfwTerminate;
 
 	return 0;
 };
