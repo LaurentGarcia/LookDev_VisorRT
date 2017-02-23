1346b7c88dcfc39e89f9b7e275700d2f9abe39ee
Lauren Garcia <laurent.garcia.carro@gmail.com>
2017-02-22 21:07:39 -0600

    Beginning with textures support

--- OpenGL_LookDevVisor/main.cpp
+++ OpenGL_LookDevVisor/main.cpp
@@ -3,6 +3,7 @@
 #define  GLEW_STATIC
 #include <glew.h>
 #include <glfw3.h>
+#include <SOIL.h>
 #include "Shaders_Manager.h"
 
 #define DEBUG TRUE
@@ -28,11 +29,23 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 
 
-GLfloat Vertices[] = { 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // Bottom Right
-					  -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // Bottom Left
-					   0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // Top 
+GLfloat Vertices[] = {
+	// Positions          // Colors           // Texture Coords
+	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top Left 
+};
+GLuint indices[] = {  // Note that we start from 0!
+	0, 1, 3, // First Triangle
+	1, 2, 3  // Second Triangle
 };
 
+GLfloat texCoords[] = {
+	0.0f, 0.0f,  // Lower-left corner  
+	1.0f, 0.0f,  // Lower-right corner
+	0.5f, 1.0f   // Top-center corner
+};
 
 int main()
 {
@@ -71,18 +84,19 @@ int main()
 #if DEBUG
 	ShaderManager.printVertexAttributes();
 #endif
-	bool shaderesult;
+	bool shaderesult = false;
 	ShaderManager.loadShader(vertexShaderFile, fragmentshaderfile, &shaderesult);
 	if (shaderesult) {
 		std::cout << "Vertex shader and Fragment Shader Load: OK" << std::endl;
 	};
 
 	//Creating buffer for Vertex loading
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	//Init Vertex Array, VBO and EBO
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	//Creating the pipeline
 	//1. Bind Vtx Array
@@ -91,15 +105,33 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 	//3.-
-	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	//Binding Buffer and Shader inputs.
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 	glBindVertexArray(0);
 
+
+	// Texture Code // To Migrate
+	int widthTex, heightTex;
+
+	unsigned char* image = SOIL_load_image("\textures\cointainer.jpg", &widthTex,&heightTex,0,SOIL_LOAD_RGB);
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image);
+
 	// APP LOOP
+
+
 
 	while (!glfwWindowShouldClose(window)){
 		glfwPollEvents();
@@ -107,13 +139,14 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		//rendering commands
 		glUseProgram(ShaderManager.getShader());
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 		glfwSwapBuffers(window);
 	};
 
-	glfwTerminate;
+	glfwTerminate();
 
 	return 0;
 };
